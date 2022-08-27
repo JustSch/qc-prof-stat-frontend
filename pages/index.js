65a58fffdcbaadfc2a6030ccb5ebc87630b68e19
Justin Schreiber <jschreiber234@gmail.com>
2022-08-27 13:17:41 -0400

    no longer uses onchange in order to limit queries

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup'
 import { Container } from 'react-bootstrap';
-import { useRef, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import Button from 'react-bootstrap/Button';
+import { useRef, useEffect, useState } from 'react';
 import useSearchResult from '../js/useSearchResult';
-import createGroupSearchResult from '../js/createGroupSearchResults'
+import createGroupSearchResult from '../js/createGroupSearchResults';
 import { useRouter } from 'next/router';
 
 
 export default function Home() {
   let router = useRouter();
-   
-  const {data: resultJSON, error}= useSearchResult(router.query.q);
+
+  const { data: resultJSON, error } = useSearchResult(router.query.q);
   const searchRef = useRef(null);
+  const [searchValue, setSearchValue] = useState('');
   useEffect(() => {
     if (searchRef.current.value === '' && router.query.q) {
       searchRef.current.value = router.query.q;
@@ -28,8 +32,13 @@ export default function Home() {
             aria-label="search"
             aria-describedby="search-addon1"
             ref={searchRef}
-            onChange={(e) => { changeQuery(e, router) }}
-          />         
+            onChange={(e) => { setSearchValue(e.target.value) }}
+            onKeyDown={(e) => { if (typeof window !== 'undefined' && e.key === 'Enter') { changeQuery(searchValue, router) } }}
+          />
+          <Button variant="outline-secondary" id="button-addon1" onClick={() => { changeQuery(searchValue, router) }}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </Button>
+
         </InputGroup>
         <div id="searchbox" className=''>
           {error && <div className="alert alert-danger" role="alert">{error}</div>}
@@ -42,6 +51,6 @@ export default function Home() {
 }
 
 const changeQuery = (e, router) => {
-  const href = `/?q=${e.target.value}`;
+  const href = `/?q=${e}`;
   router.push(href, href, { shallow: true });
 }
